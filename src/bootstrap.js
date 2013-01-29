22fe93fd5be2ebfaacfb00cee0deae961d4af544
James Keane <james.keane.github@gmail.com>
2013-01-28 21:21:48 -0500

    We are bootstrapped!!!!

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,44 @@
 var lang = require('./lang'),
     fs = require('fs'),
-    _ = require('underscore'),
+    _ = require('underscore');
 
-    ASTNode    = require('./compiler/node'),
-    array      = require('./compiler/array'),
-    functional = require('./compiler/functional'),
-    oop        = require('./compiler/oop');
+// ASTNode
+// we will cache all compiled templates, so we don't have to read and recompile them
+var template_cache = {};
 
-lang.Parser.HamFile = new ASTNode.extend({
+var ASTNode = {
+  template: '',
+
+  _initialize: function() {
+
+  },
+
+  serialize: function() {
+    return {};
+  },
+
+  toJS: function(state) {
+    var data = this.serialize(state);
+    if(typeof data === 'string') return data;
+
+    // read the template from cache or fs
+    var tmpl = template_cache[this.template];
+    if(tmpl === undefined) {
+      tmpl = _.template(fs.readFileSync(__dirname + '/compiler/templates/' + this.template + '.ejs', 'utf8'));
+      template_cache[this.template] = tmpl;
+    }
+
+    return tmpl(data);
+  }
+};
+
+ASTNode.extend = function(o) {
+  return _.defaults(o, ASTNode);
+};
+
+
+// AST Transformers
+lang.Parser.HamFile = ASTNode.extend({
   template: 'prologue',
 
   serialize: function() {
@@ -91,17 +122,158 @@ lang.Parser.Import = {
   }
 }
 
-lang.Parser.ArrayAccess = array.ArrayAccess;
-lang.Parser.ArraySlice = array.ArraySlice;
-lang.Parser.ArrayNode = array.ArrayDef;
-lang.Parser.ArrayRange = array.ArrayRange;
+lang.Parser.ArrayAccess = ASTNode.extend({
+  toJS: function(state) {
+    var accessor = this.elements[2].toJS();
 
-lang.Parser.Lambda = functional.Lambda;
-lang.Parser.ListComprehension = functional.ListComprehension;
-lang.Parser.FunctionInvocation = functional.FunctionInvocation;
+    return '[' + accessor + ']';
+  }
+});
 
-lang.Parser.PrototypeExpander = oop.PrototypeExpander;
-lang.Parser.ClassDef = oop.ClassDef;
+lang.Parser.ArraySlice = ASTNode.extend({
+  toJS: function(state) {
+    var ret = '';
+
+    // slice
+    var start = this.elements[2].start.textValue,
+        end   = this.elements[2].end.textValue,
+        step  = this.elements[2].step.integer;
+
+    start = start === '' ? '0' : start;
+    if(start !== '0' || end !== '') {
+      ret += '.slice('+start
+      if(end !== '') ret += ', '+end
+      ret += ')'
+    }
+    
+    if(step) {
+      ret += '.step(' + step.textValue + ')'
+    }
+    return ret;
+  }
+});
+
+lang.Parser.ArrayRange = ASTNode.extend({
+  template: 'array_range', 
+
+  serialize: function(state) {
+    return {
+      start: this.start.textValue,
+      end: this.end.textValue
+    };
+  }
+});
+
+lang.Parser.ArrayNode = ASTNode.extend({
+  toJS: function(state, indent) {
+    if(this.elements[2].textValue === '') {
+      return "[]"
+    }
+
+    var first = this.elements[2];
+    var acc = [first.expr.elements[0].toJS(state, indent+1)];
+
+    first.elements[1].elements.forEach(function(node) {
+      acc.push(node.expr.elements[0].toJS(state, indent+1));
+    });
+
+    return "[" + acc.join(', ') + "]";
+  }
+});
+
+lang.Parser.ListComprehension = ASTNode.extend({
+  template: 'comprehension',
+
+  serialize: function(state) {
+    // collect params and providers
+    var params = [];
+    var providers = [];
+
+    var first = this.elements[6];
+    params.push(first.ident_p.textValue);
+    providers.push(first.expr.toJS());
+
+    this.elements.slice(7, -2).forEach(function(p) {
+      if(p.textValue === '') return;
+      p = p.elements[0].elements[3];
+      if(p.ident_p === undefined || p.expr == undefined) return;
+      
+      params.push(p.ident_p.textValue);
+      providers.push(p.expr.toJS());
+    });
+
+    return {
+      params: params,
+      providers: providers,
+      body: this.expr.toJS()
+    }
+  }
+});
+
+lang.Parser.Lambda = ASTNode.extend({
+  template: 'lambda',
+  
+  serialize: function(state, indent) {
+    // get the params
+    var params = [];
+
+    // Is it a no param list lambda?
+    if(this.elements[0].textValue !== '') {
+      var first = this.elements[0].elements[2].ident_p;
+      if(first !== undefined) {
+        params.push(first.textValue);
+        var it = this.elements[0].elements[2].elements[1];
+        it.forEach(function(el) {
+          params.push(el.ident_p.elements[0].textValue);
+        });
+      }
+    }
+
+    return {
+      params: params,
+      body: this.funblock.toJS(state)
+    }
+  }
+});
+
+lang.Parser.FunctionInvocation = ASTNode.extend({
+  serialize: function(state) {
+    if(this.lambda) {
+      return "(" + this.lambda.toJS(state) + ")";
+    }
+
+    if(this.elements[2].textValue === '') {
+      return "()";
+    }
+
+    return "(" + this.elements[2].toJS(state) + ")";
+  }
+});
+
+
+lang.Parser.ClassDef = ASTNode.extend({
+  template: 'class',
+
+  serialize: function(state) {
+    var parent = this.elements[4].ident ? this.elements[4].ident.toJS(state) : 'Object';
+
+    return {
+      name: this.ident.toJS(state),
+      parent: parent,
+      body: this.elements[5].toJS(state)
+    }
+  }
+});
+
+lang.Parser.PrototypeExpander = ASTNode.extend({
+  serialize: function() {
+    var ret = this.ident.toJS();
+    if(this.elements[1].ident_p)  {
+      ret += '.prototype.' + this.elements[1].ident_p.textValue;
+    }
+    return ret;
+  }
+});
 
 lang.Parser.ExprList = {
   toJS: function(state) {
--- src/main.js
+++ src/main.js
@@ -4,13 +4,19 @@
 var fs = require('fs'),
     path = require('path'),
     vm = require('vm'),
-    ham = require('./ham');
+    bootstrap = require('./bootstrap'),
+    ham = null;
 
 // hook into the require() system
 require.extensions['.ham'] = function(module, filename) {
-  var js = ham.parse(fs.readFileSync(filename, 'utf8')).toJS();
-  module._compile(js, filename);
+  var compiler = ham || bootstrap;
+
+  var js = compiler.parse(fs.readFileSync(filename, 'utf8'));
+  module._compile(js.toJS(), filename);
 };
+
+// first compile the ham compiler
+ham = require('./ham');
 
 // require the file into this scope
 require(process.cwd() + '/' + process.argv[2]);
